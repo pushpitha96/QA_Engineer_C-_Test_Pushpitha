ba5b15e26d8f5214ba6dfe6f8798ff33a971ee9d
a_pushpitha <a.pushpitha@Directfn.com>
2024-06-11 12:58:01 +0530

    Updated

--- RestfulApiTests.cs
+++ RestfulApiTests.cs
@@ -8,13 +8,16 @@ using System.Text.Json;
 
 namespace QA_Code_test
 {
-    public class RestfulApiTests
+    public class RestfulApiTests : IClassFixture<TestFixture>
     {
         private const string BaseUrl = "https://api.restful-api.dev/";
+        private readonly TestFixture _fixture;
 
-        /// <summary>
-        /// Test to get the list of all objects from the API.
-        /// </summary>
+        public RestfulApiTests(TestFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
         [Fact]
         public async Task GetAllObjects_ShouldReturnListOfObjects()
         {
@@ -22,18 +25,15 @@ namespace QA_Code_test
             var request = new RestRequest("objects", Method.Get);
             var response = await client.ExecuteAsync(request);
 
-            // Verify the response status code is OK (200)
+            // Debugging statement
+            Console.WriteLine("GetAllObjects response content: " + response.Content);
+
             response.StatusCode.Should().Be(HttpStatusCode.OK);
-            // Verify the response content is not empty
             response.Content.Should().NotBeNullOrEmpty();
-            // Additional verification to ensure response contains a list of objects
             response.Content.Should().Contain("\"id\"");
             response.Content.Should().Contain("\"name\"");
         }
 
-        /// <summary>
-        /// Test to add a new object using POST.
-        /// </summary>
         [Fact]
         public async Task AddObject_ShouldReturnSuccess()
         {
@@ -46,67 +46,35 @@ namespace QA_Code_test
             });
             var response = await client.ExecuteAsync(request);
 
-            // Verify the response status code is either Created (201) or OK (200)
+            // Debugging statement
+            Console.WriteLine("AddObject response content: " + response.Content);
+
             response.StatusCode.Should().BeOneOf(HttpStatusCode.Created, HttpStatusCode.OK);
-            // Verify the response content contains the added object's name
             response.Content.Should().Contain("NewObject");
+
+            _fixture.AddedObjectId = ExtractIdFromResponse(response.Content);
         }
 
-
-        /// <summary>
-        /// Test to get a single object using the ID of the object added in the previous test.
-        /// </summary>
         [Fact]
         public async Task GetSingleObject_ShouldReturnCorrectObject()
         {
             var client = new RestClient(BaseUrl);
-            var addRequest = new RestRequest("objects", Method.Post);
-            addRequest.AddHeader("Content-Type", "application/json");
-            addRequest.AddHeader("User-Agent", "Mozilla/5.0");
-            addRequest.AddJsonBody(new
-            {
-                name = "ObjectToGet",
-                data = new
-                {
-                    color = "Red",
-                    capacity = "128 GB"
-                }
-            });
-            var addResponse = await client.ExecuteAsync(addRequest);
-            var addedObjectId = ExtractIdFromResponse(addResponse.Content);
-
-            var getRequest = new RestRequest($"objects/{addedObjectId}", Method.Get);
+            var getRequest = new RestRequest($"objects/{_fixture.AddedObjectId}", Method.Get);
             var getResponse = await client.ExecuteAsync(getRequest);
 
-            // Verify the response status code is OK (200)
+            // Debugging statements
+            Console.WriteLine("GetSingleObject request URI: " + getRequest.Resource);
+            Console.WriteLine("GetSingleObject response content: " + getResponse.Content);
+
             getResponse.StatusCode.Should().Be(HttpStatusCode.OK);
-            // Verify the response content contains the object's name
-            getResponse.Content.Should().Contain("ObjectToGet");
+            getResponse.Content.Should().Contain("NewObject");
         }
 
-        /// <summary>
-        /// Test to update the object added in the previous test using PUT.
-        /// </summary>
         [Fact]
         public async Task UpdateObject_ShouldReturnUpdatedObject()
         {
             var client = new RestClient(BaseUrl);
-            var addRequest = new RestRequest("objects", Method.Post);
-            addRequest.AddHeader("Content-Type", "application/json");
-            addRequest.AddHeader("User-Agent", "Mozilla/5.0");
-            addRequest.AddJsonBody(new
-            {
-                name = "ObjectToUpdate",
-                data = new
-                {
-                    color = "Green",
-                    capacity = "64 GB"
-                }
-            });
-            var addResponse = await client.ExecuteAsync(addRequest);
-            var addedObjectId = ExtractIdFromResponse(addResponse.Content);
-
-            var updateRequest = new RestRequest($"objects/{addedObjectId}", Method.Put);
+            var updateRequest = new RestRequest($"objects/{_fixture.AddedObjectId}", Method.Put);
             updateRequest.AddHeader("Content-Type", "application/json");
             updateRequest.AddHeader("User-Agent", "Mozilla/5.0");
             updateRequest.AddJsonBody(new
@@ -120,47 +88,46 @@ namespace QA_Code_test
             });
             var updateResponse = await client.ExecuteAsync(updateRequest);
 
-            // Verify the response status code is OK (200)
+            // Debugging statements
+            Console.WriteLine("UpdateObject request URI: " + updateRequest.Resource);
+            Console.WriteLine("UpdateObject response content: " + updateResponse.Content);
+
             updateResponse.StatusCode.Should().Be(HttpStatusCode.OK);
-            // Verify the response content contains the updated object's name
             updateResponse.Content.Should().Contain("UpdatedObject");
         }
 
-        /// <summary>
-        /// Test to delete the object using DELETE.
-        /// </summary>
         [Fact]
         public async Task DeleteObject_ShouldReturnSuccess()
         {
             var client = new RestClient(BaseUrl);
-            var addRequest = new RestRequest("objects", Method.Post);
-            addRequest.AddJsonBody(new { name = "ObjectToDelete", data = new { Generation = "4th", Price = "519.99", Capacity = "256 GB" } });
-            var addResponse = await client.ExecuteAsync(addRequest);
-            var addedObjectId = ExtractIdFromResponse(addResponse.Content);
-
-            var deleteRequest = new RestRequest($"objects/{addedObjectId}", Method.Delete);
+            var deleteRequest = new RestRequest($"objects/{_fixture.AddedObjectId}", Method.Delete);
             var deleteResponse = await client.ExecuteAsync(deleteRequest);
 
-            // Verify the response status code is either NoContent (204) or OK (200)
+            // Debugging statements
+            Console.WriteLine("DeleteObject delete request URI: " + deleteRequest.Resource);
+            Console.WriteLine("DeleteObject delete response content: " + deleteResponse.Content);
+
             deleteResponse.StatusCode.Should().BeOneOf(HttpStatusCode.NoContent, HttpStatusCode.OK);
 
-            var getRequest = new RestRequest($"objects/{addedObjectId}", Method.Get);
+            var getRequest = new RestRequest($"objects/{_fixture.AddedObjectId}", Method.Get);
             var getResponse = await client.ExecuteAsync(getRequest);
 
-            // Verify the response status code is NotFound (404)
+            // Debugging statements
+            Console.WriteLine("DeleteObject get request URI: " + getRequest.Resource);
+            Console.WriteLine("DeleteObject get response content: " + getResponse.Content);
+
             getResponse.StatusCode.Should().Be(HttpStatusCode.NotFound);
         }
 
-        /// <summary>
-        /// Helper method to extract the ID from the response content.
-        /// </summary>
-        /// <param name="responseContent">The response content in JSON format.</param>
-        /// <returns>The extracted ID as a string.</returns>
         private string ExtractIdFromResponse(string responseContent)
         {
-            // Parse the JSON response content to extract the ID
             var json = JsonDocument.Parse(responseContent);
             return json.RootElement.GetProperty("id").GetString();
         }
+    }
+
+    public class TestFixture
+    {
+        public string AddedObjectId { get; set; }
     }
 }
--- obj/Debug/net8.0/QA Code test.AssemblyInfo.cs
+++ obj/Debug/net8.0/QA Code test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QA Code test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c19304e6c4b0803b08b73c5db37b6979c236d2d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("QA Code test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QA Code test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
